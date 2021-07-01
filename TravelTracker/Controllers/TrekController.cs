30a23c369cbb0e2f6b2b5c998064450276856268
auris-16D <nigesurtees@gmail.com>
2021-07-01 22:56:55 +0100

    revert guid change

--- TravelTracker/Controllers/TrekController.cs
+++ TravelTracker/Controllers/TrekController.cs
@@ -69,7 +69,7 @@ namespace TravelTracker.Controllers
             {
                 trek = new Trek
                 {
-                    Id = Guid.Parse(trekRequest.OwnerId),
+                    Id = Guid.NewGuid(),
                     StartTime = new DateTimeOffset(trekRequest.StartTime).ToUnixTimeMilliseconds(),
                     FinishTime = new DateTimeOffset(trekRequest.FinishTime).ToUnixTimeMilliseconds(),
                     Area = trekRequest.Area,
